2a828d4a55637cc0ad236f4e5c4739359ced7b4d
Alphazoya <112188066+Alphazoya@users.noreply.github.com>
2022-08-26 20:10:49 +0500

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -7,7 +7,7 @@
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
 //▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
+//▮WHATSAPP US : +923306116340
 //▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
